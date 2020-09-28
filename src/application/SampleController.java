51386cd55e46886fc3fc6c1c0b5e2680f5c6cc22
mathnm <40720588+mathnm@users.noreply.github.com>
2020-09-28 18:54:03 -0300

    Initial commit

---
+++ src/application/SampleController.java
@@ -0,0 +1,106 @@
+package application;
+
+import java.io.File;
+import java.text.DecimalFormat;
+
+import caracteristicas.Extrator;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+
+public class SampleController {
+
+	@FXML
+	private ImageView imageView;
+	
+	@FXML
+	public TextField classe;
+	@FXML
+	public TextField probabilidade;
+	@FXML
+	public TextField roupaLisa;
+	@FXML
+	public TextField cabeloLisa;
+	@FXML
+	public TextField sapatoLisa;
+	@FXML
+	public TextField cabeloNed;
+	@FXML
+	public TextField sueterNed;
+	@FXML
+	public TextField camisaNed;
+	
+	@FXML
+	public void extrairCaracteristicas() {
+		Extrator.extrair();
+	}
+	
+	double[] bayes = new double[2];
+	
+	@FXML
+	public void selecionaImagem() {
+		File f = buscaImg();
+		if(f != null) {
+			Image img = new Image(f.toURI().toString());
+			imageView.setImage(img);
+			imageView.setFitWidth(img.getWidth());
+			imageView.setFitHeight(img.getHeight());
+			double[] caracteristicas = Extrator.extraiCaracteristicas(f);
+			for (double d : caracteristicas) {
+				System.out.println(d);
+			}
+			
+			bayes = Extrator.naiveBayes(caracteristicas);
+			
+			DecimalFormat df = new DecimalFormat("###.##");
+			
+			if(bayes[1] > bayes[0]) {
+				classe.setText("Ned Flanders");
+				probabilidade.setText(df.format(bayes[1]*100)+"%");
+			} else {
+				classe.setText("Lisa Simpson");
+				probabilidade.setText(df.format(bayes[0]*100)+"%");
+			}
+			
+			roupaLisa.setText(""+caracteristicas[0]);
+			cabeloLisa.setText(""+caracteristicas[1]);
+			sapatoLisa.setText(""+caracteristicas[2]);
+			cabeloNed.setText(""+caracteristicas[3]);
+			sueterNed.setText(""+caracteristicas[4]);
+			camisaNed.setText(""+caracteristicas[5]);
+			
+			/* caracteristicas[0] = roupaLaranjaLisa;
+        caracteristicas[1] = cabeloAmareloLisa;
+        caracteristicas[2] = sapatoLaranjaLisa;
+        caracteristicas[3] = cabeloMarromNed;
+        caracteristicas[4] = sueterVerdeNed;
+        caracteristicas[5] = camisaRosaNed;*/
+			
+//			System.out.println("chance de ser lisa: "+bayes[0]);
+//			System.out.println("chance de ser ned: "+bayes[1]);
+			
+		}
+	}
+	
+	private File buscaImg() {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.getExtensionFilters().add(new 
+				   FileChooser.ExtensionFilter(
+						   "Imagens", "*.jpg", "*.JPG", 
+						   "*.png", "*.PNG", "*.gif", "*.GIF", 
+						   "*.bmp", "*.BMP")); 	
+		 fileChooser.setInitialDirectory(new File("src/imagens"));
+		 File imgSelec = fileChooser.showOpenDialog(null);
+		 try {
+			 if (imgSelec != null) {
+			    return imgSelec;
+			 }
+		 } catch (Exception e) {
+			e.printStackTrace();
+		 }
+		 return null;
+	}
+	
+}
---
+++ src/caracteristicas/Extrator.java
@@ -0,0 +1,232 @@
+package caracteristicas;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import org.opencv.core.Mat;
+import org.opencv.highgui.HighGui;
+import org.opencv.imgcodecs.Imgcodecs;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.PixelReader;
+import javafx.scene.paint.Color;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.core.DenseInstance;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
+public class Extrator {
+
+public static double[] extraiCaracteristicas(File f) {
+		
+		double[] caracteristicas = new double[7];
+		
+		double roupaLaranjaLisa = 0;
+		double cabeloAmareloLisa = 0;
+		double sapatoLaranjaLisa = 0;
+		double cabeloMarromNed = 0;
+		double sueterVerdeNed = 0;
+		double camisaRosaNed = 0; 
+		
+		
+		Image img = new Image(f.toURI().toString());
+		PixelReader pr = img.getPixelReader();
+		
+		Mat imagemOriginal = Imgcodecs.imread(f.getPath());
+        Mat imagemProcessada = imagemOriginal.clone();
+		
+		int w = (int)img.getWidth();
+		int h = (int)img.getHeight();
+		
+		
+		for(int i=0; i<h; i++) {
+			for(int j=0; j<w; j++) {
+				
+				Color cor = pr.getColor(j,i);
+				
+				double r = cor.getRed()*255; 
+				double g = cor.getGreen()*255;
+				double b = cor.getBlue()*255;
+				
+				if(i < h/2 && isCabeloMarromNed(r, g, b)) {
+					cabeloMarromNed++;
+					imagemProcessada.put(i, j, new double[]{0, 255, 128});
+				}
+				if(isSueterVerdeNed(r, g, b)) {
+					sueterVerdeNed++;
+					imagemProcessada.put(i, j, new double[]{0, 255, 128});
+				}
+				if (isCamisaRosaNed(r, g, b)) {
+					camisaRosaNed++;
+					imagemProcessada.put(i, j, new double[]{0, 255, 128});
+				}
+				if(isRoupaLaranjaLisa(r, g, b)) {
+					roupaLaranjaLisa++;
+					imagemProcessada.put(i, j, new double[]{0, 255, 255});
+				}
+				if(i < (h/2 - h/3) && isCabeloAmareloLisa(r, g, b)) {
+					cabeloAmareloLisa++;
+					imagemProcessada.put(i, j, new double[]{0, 255, 255});
+				}
+				if (i < (h/2 + h/3) && isSapatoLaranjaLisa(r, g, b)) {
+					sapatoLaranjaLisa++;
+					imagemProcessada.put(i, j, new double[]{0, 255, 255});
+				}
+				
+			}
+		}
+		
+		// Normaliza as características pelo número de pixels totais da imagem para %
+		roupaLaranjaLisa = (roupaLaranjaLisa / (w * h)) * 100;
+		cabeloAmareloLisa = (cabeloAmareloLisa / (w * h)) * 100;
+		sapatoLaranjaLisa = (sapatoLaranjaLisa / (w * h)) * 100;
+		cabeloMarromNed = (cabeloMarromNed / (w * h)) * 100;
+		sueterVerdeNed = (sueterVerdeNed / (w * h)) * 100;
+		camisaRosaNed = (camisaRosaNed / (w * h)) * 100;
+        
+        caracteristicas[0] = roupaLaranjaLisa;
+        caracteristicas[1] = cabeloAmareloLisa;
+        caracteristicas[2] = sapatoLaranjaLisa;
+        caracteristicas[3] = cabeloMarromNed;
+        caracteristicas[4] = sueterVerdeNed;
+        caracteristicas[5] = camisaRosaNed;
+        //APRENDIZADO SUPERVISIONADO - JÁ SABE QUAL A CLASSE NAS IMAGENS DE TREINAMENTO
+        caracteristicas[6] = f.getName().charAt(4)=='1'?1:0;
+		
+//		HighGui.imshow("Imagem original", imagemOriginal);
+//		HighGui.imshow("Imagem processada", imagemProcessada);
+//        
+//        HighGui.waitKey(0);
+        
+		return caracteristicas;
+	}
+	
+	public static boolean isRoupaLaranjaLisa(double r, double g, double b) {
+		 if (b >= 0 && b <= 50 &&  g >= 25 && g <= 90 &&  r >= 125 && r <= 240) {                       
+         	return true;
+         }
+		 return false;
+	}
+	public static boolean isCabeloAmareloLisa(double r, double g, double b) {
+		if (b >= 0 && b <= 40 &&  g >= 150 && g <= 220 &&  r >= 170 && r <= 250) {                       
+			return true;
+		}
+		return false;
+	}
+	public static boolean isSapatoLaranjaLisa(double r, double g, double b) {
+		if (b >= 0 && b <= 50 &&  g >= 25 && g <= 90 &&  r >= 125 && r <= 240) {                       
+			return true;
+		}
+		return false;
+	}
+	public static boolean isCabeloMarromNed(double r, double g, double b) {
+		if (b >= 0 && b <= 70 &&  g >= 50 && g <= 90 &&  r >= 65 && r <= 130) {                       
+			return true;
+		}
+		return false;
+	}
+	public static boolean isSueterVerdeNed(double r, double g, double b) {
+		if (b >= 0 && b <= 45 &&  g >= 45 && g <= 130 &&  r >= 20 && r <= 50) {                       
+			return true;
+		}
+		return false;
+	}
+	public static boolean isCamisaRosaNed(double r, double g, double b) {
+		if (b >= 140 && b <= 180 &&  g >= 115 && g <= 160 &&  r >= 185 && r <= 250) {                       
+			return true;
+		}
+		return false;
+	}
+
+
+
+	public static void extrair() {
+				
+	    // Cabeçalho do arquivo Weka
+		String exportacao = "@relation caracteristicas\n\n";
+		exportacao += "@attribute laranja_roupa_lisa real\n";
+		exportacao += "@attribute amarelo_cabelo_lisa real\n";
+		exportacao += "@attribute laranja_sapato_lisa real\n";
+		exportacao += "@attribute marrom_cabelo_ned real\n";
+		exportacao += "@attribute verde_sueter_ned real\n";
+		exportacao += "@attribute rosa_camisa_ned real\n";
+		exportacao += "@attribute classe {Lisa, Ned}\n\n";
+		exportacao += "@data\n";
+	        
+	    // Diretório onde estão armazenadas as imagens
+	    File diretorio = new File("src\\imagens");
+	    File[] arquivos = diretorio.listFiles();
+	    
+        // Definição do vetor de características
+        double[][] caracteristicas = new double[359][7];
+        
+        // Percorre todas as imagens do diretório
+        int cont = -1;
+        for (File imagem : arquivos) {
+        	cont++;
+        	caracteristicas[cont] = extraiCaracteristicas(imagem);
+        	
+        	String classe = caracteristicas[cont][6] == 0 ?"Lisa":"Ned";
+        	
+        	System.out.println("Laranja roupa Lisa: " + caracteristicas[cont][0] 
+            		+ " - Amarelo cabelo Lisa: " + caracteristicas[cont][1] 
+            		+ " - Laranja sapato Lisa: " + caracteristicas[cont][2] 
+            		+ " - Marrom cabelo Ned: " + caracteristicas[cont][3] 
+            		+ " - Verde sueter Ned: " + caracteristicas[cont][4] 
+            		+ " - Rosa camisa Ned: " + caracteristicas[cont][5] 
+            		+ " - Classe: " + classe);
+        	
+        	exportacao += caracteristicas[cont][0] + "," 
+                    + caracteristicas[cont][1] + "," 
+        		    + caracteristicas[cont][2] + "," 
+                    + caracteristicas[cont][3] + "," 
+        		    + caracteristicas[cont][4] + "," 
+                    + caracteristicas[cont][5] + "," 
+                    + classe + "\n";
+        }
+        
+     // Grava o arquivo ARFF no disco
+        try {
+        	File arquivo = new File("caracteristicas.arff");
+        	FileOutputStream f = new FileOutputStream(arquivo);
+        	f.write(exportacao.getBytes());
+        	f.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		
+	}
+	
+	public static double[] naiveBayes(double[]caracteristicas) {
+		double[] retorno = {0,0};
+		try {
+			//*******carregar arquivo de características
+			DataSource ds = new DataSource("caracteristicas.arff");
+			Instances instancias = ds.getDataSet();
+			//instancias.setClassIndex(6);
+			instancias.setClassIndex(instancias.numAttributes()-1);
+			
+			//Classifica com base nas características da imagem selecionada
+			NaiveBayes nb = new NaiveBayes();
+			nb.buildClassifier(instancias);//aprendizagem (tabela de probabilidades)
+			
+			Instance novo = new DenseInstance(instancias.numAttributes());
+			novo.setDataset(instancias);
+			novo.setValue(0, caracteristicas[0]);
+			novo.setValue(1, caracteristicas[1]);
+			novo.setValue(2, caracteristicas[2]);
+			novo.setValue(3, caracteristicas[3]);
+			novo.setValue(4, caracteristicas[4]);
+			novo.setValue(5, caracteristicas[5]);
+			
+			retorno = nb.distributionForInstance(novo);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return retorno;
+	}
+	
+}
